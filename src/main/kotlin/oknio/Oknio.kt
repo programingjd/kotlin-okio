73f17683426b16d04929681a6c9f22cab36c3890
Jerome David <programingjd@gmail.com>
2017-08-04 12:23:23 +0200

    -

--- src/main/kotlin/oknio/Oknio.kt
+++ src/main/kotlin/oknio/Oknio.kt
@@ -7,9 +7,19 @@ class Oknio private constructor() {
 
     fun buffer(source: Source): BufferedSource = RealBufferedSource(source)
 
-    //fun buffer(sink: Sink): BufferedSink = RealBufferedSink(sink)
+    fun buffer(sink: Sink): BufferedSink = RealBufferedSink(sink)
 
-
+    fun blackhole(): Sink {
+      return object : Sink {
+        override fun write(source: Buffer, byteCount: Long) = source.skip(byteCount)
+        suspend override fun aWrite(source: Buffer, byteCount: Long) = source.aSkip(byteCount)
+        override fun flush() {}
+        suspend override fun aFlush() {}
+        override fun close() {}
+        suspend override fun aClose() {}
+        override fun timeout() = Timeout.NONE
+      }
+    }
 
   }
 
